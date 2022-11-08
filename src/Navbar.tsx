ee0283b1f62816a36e6cbed7b072d9443fd86944
mohamed-ibrahim-01 <mohamed.ibrahim02023@gmail.com>
2022-11-09 00:45:27 +0200

    feat: navbar with search & logo & notification

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,21 +1,78 @@
-import { createStyles, Input } from "@mantine/core"
-import { IconSearch } from "@tabler/icons"
+import { forwardRef } from "react"
+import { createStyles, Group, Input, Avatar, Tooltip } from "@mantine/core"
+import { IconSearch, IconNotification } from "@tabler/icons"
+import messi from "./assets/messi.jpg"
+import logo from "./assets/logo.svg"
+import { FlexSpace } from "./shared"
 
 const useStyles = createStyles((theme, _params, getRef) => ({
+    navbar: {
+        height: "50px",
+    },
     wrapper: {
         maxWidth: 215,
     },
+    input: {},
+    alert_notification: {
+        ref: getRef("notifi"),
+        [`& circle`]: {
+            fill: "red",
+            stroke: "red",
+        },
+    },
 }))
 
-export const Search = () => {
-    const { classes } = useStyles()
+const buildStyles = (params?: any) => {
+    const { classes, cx, theme } = useStyles(params)
+    const styles = {
+        navbar: cx(classes.navbar, "px-3 shadow-md"),
+        tooltip: cx("text-xs"),
+    }
+    return { styles, classes, cx, theme }
+}
+
+const Notification = forwardRef<HTMLDivElement>(function Notification(props, ref) {
+    const { classes } = buildStyles()
+    return (
+        <div ref={ref} {...props}>
+            <IconNotification
+                size="30px"
+                className={classes.alert_notification}
+                color={"#333333"}
+            />
+        </div>
+    )
+})
+
+export const Navbar = () => {
+    const { styles } = buildStyles()
+
+    return (
+        <Group spacing="lg" className={styles.navbar} align="center">
+            <img alt="Platform Logo" src={logo} style={{ width: "38px", height: "38px" }} />
+            <Search />
+            <FlexSpace />
+            <Tooltip
+                classNames={{ tooltip: styles.tooltip }}
+                label="Notification"
+                position="bottom"
+            >
+                <Notification />
+            </Tooltip>
+            <Avatar src={messi} radius="xl" size="md" />
+        </Group>
+    )
+}
+
+const Search = () => {
+    const { classes } = buildStyles()
     return (
         <Input
-            classNames={{ wrapper: classes.wrapper }}
-            rightSection={<IconSearch color="#828282" />}
+            size="xs"
+            classNames={{ wrapper: classes.wrapper, input: classes.input }}
+            rightSection={<IconSearch size="20px" color="#828282" />}
             placeholder="Search"
             radius="md"
-            size="xs"
         />
     )
 }
---
+++ src/shared.tsx
@@ -0,0 +1,3 @@
+export const FlexSpace = () => {
+    return <div className="flex-1"></div>
+}
