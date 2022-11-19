9e4b274b04d8c9c5bbf0ce5d5921f3879bce5efb
mohamed-ibrahim-01 <mohamed.ibrahim02023@gmail.com>
2022-11-19 16:11:09 +0200

    feat: react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import { Home } from "./Home"
+import { Navbar } from "./Navbar"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
-    return <Home />
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/navbar" element={<Navbar />} />
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App
