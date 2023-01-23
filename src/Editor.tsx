8bff60fd5efeee341a8d1cedfa157d3b7c98ff28
mohamed-ibrahim-01 <mohamed.ibrahim02023@gmail.com>
2023-01-23 06:01:10 +0200

    feat: lists & block quote

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -6,40 +6,42 @@ import Underline from "@tiptap/extension-underline"
 import History from "@tiptap/extension-history"
 import Document from "@tiptap/extension-document"
 import Paragraph from "@tiptap/extension-paragraph"
+import BulletList from "@tiptap/extension-bullet-list"
+import OrderedList from "@tiptap/extension-ordered-list"
+import Blockquote from "@tiptap/extension-blockquote"
+import ListItem from "@tiptap/extension-list-item"
 import { Heading, Level } from "@tiptap/extension-heading"
 import TextExtension from "@tiptap/extension-text"
+import * as constants from "./constants"
 
 import { ActionIcon, Button, Group, Stack, Menu, Kbd } from "@mantine/core"
 import {
     IconArrowBackUp,
     IconArrowForwardUp,
+    IconBlockquote,
     IconBold,
     IconChevronDown,
     IconHeading,
+    IconIndentDecrease,
+    IconIndentIncrease,
     IconItalic,
+    IconList,
+    IconListNumbers,
     IconUnderline,
 } from "@tabler/icons"
 
 export const LessonEditor = () => {
     // When useEdior is null ?
     const editor = useEditor({
-        extensions: [Document, Paragraph, TextExtension, Bold, Heading, Italic, Underline, History],
-        content: `
-            <h1>Typography should be easy</h1>
-            <p></p>
-            <p>So that's a header for you — with any luck if we've done our job correctly that will look pretty reasonable.</p>
-            <p></p>
-
-            <p><strong>Something a wise person once told me about typography is:</strong></p>
-            <p></p>
-
-            Typography is pretty important if you don't want your stuff to look like trash. Make it good then it won't be bad.
-
-            It's probably important that images look okay here by default as well:
-      `,
+        // prettier-ignore
+        extensions: [
+            Document, Paragraph, TextExtension, Bold, Heading,Italic, Underline,
+            History, BulletList, ListItem, OrderedList, Blockquote
+        ],
+        content: constants.EDITOR_SAMPLE,
         editorProps: {
             attributes: {
-                class: "max-w-max prose prose-sm [&>*]:m-0 focus:outline-none",
+                class: "max-w-max prose prose-sm [&_*]:m-0 focus:outline-none",
             },
         },
     })
@@ -64,21 +66,33 @@ const EditorContainer = ({ children }: { children: React.ReactNode }) => {
 }
 
 const MENU_ACTIONS = (editor: Editor) => [
-    { action: editor.chain().focus().toggleBold().run, EditorIcon: IconBold },
-    { action: editor.chain().focus().toggleItalic().run, EditorIcon: IconItalic },
-    { action: editor.chain().focus().toggleUnderline().run, EditorIcon: IconUnderline },
-    { action: editor.chain().focus().undo().run, EditorIcon: IconArrowBackUp },
-    { action: editor.chain().focus().redo().run, EditorIcon: IconArrowForwardUp },
+    { action: editor.chain().focus().toggleBold().run, EditorActionIcon: IconBold },
+    { action: editor.chain().focus().toggleItalic().run, EditorActionIcon: IconItalic },
+    { action: editor.chain().focus().toggleUnderline().run, EditorActionIcon: IconUnderline },
+    { action: editor.chain().focus().undo().run, EditorActionIcon: IconArrowBackUp },
+    { action: editor.chain().focus().redo().run, EditorActionIcon: IconArrowForwardUp },
+    { action: editor.chain().focus().toggleBulletList().run, EditorActionIcon: IconList },
+    { action: editor.chain().focus().toggleOrderedList().run, EditorActionIcon: IconListNumbers },
+    {
+        action: editor.chain().focus().liftListItem("listItem").run,
+        EditorActionIcon: IconIndentDecrease,
+    },
+    {
+        action: editor.chain().focus().sinkListItem("listItem").run,
+        EditorActionIcon: IconIndentIncrease,
+    },
+    { action: editor.chain().focus().toggleBlockquote().run, EditorActionIcon: IconBlockquote },
+    { action: editor.chain().focus().toggleBlockquote().run, EditorActionIcon: IconBlockquote },
 ]
 
 const EditorMenu = ({ editor }: { editor: Editor }) => {
     return (
         <Group className="gap-0 shadow-sm flex-[0_0_auto]">
             <HeadingMenu editor={editor} />
-            {MENU_ACTIONS(editor).map(({ action, EditorIcon }, i) => {
+            {MENU_ACTIONS(editor).map(({ action, EditorActionIcon }, i) => {
                 return (
                     <ActionIcon onClick={action} key={i}>
-                        <EditorIcon size="18" />
+                        <EditorActionIcon size="18" />
                     </ActionIcon>
                 )
             })}
@@ -86,25 +100,14 @@ const EditorMenu = ({ editor }: { editor: Editor }) => {
     )
 }
 
-const HEADINGS: React.ReactNode[] = [
-    <h1 key="heading_1" className="m-0">
-        Heading 1
-    </h1>,
-    <h2 key="heading_2" className="m-0">
-        Heading 2
-    </h2>,
-    <h3 key="heading_3" className="m-0">
-        Heading 3
-    </h3>,
-    <h4 key="heading_4" className="m-0">
-        Heading 4
-    </h4>,
-    <h5 key="heading_5" className="m-0">
-        Heading 5
-    </h5>,
-    <h6 key="heading_6" className="m-0">
-        Heading 6
-    </h6>,
+// prettier-ignore
+const HEADINGS: React.ReactNode[] = [ 
+    <h1 key="heading_1" className="m-0"> Heading 1 </h1>,
+    <h2 key="heading_2" className="m-0"> Heading 2 </h2>,
+    <h3 key="heading_3" className="m-0"> Heading 3 </h3>,
+    <h4 key="heading_4" className="m-0"> Heading 4 </h4>,
+    <h5 key="heading_5" className="m-0"> Heading 5 </h5>,
+    <h6 key="heading_6" className="m-0"> Heading 6 </h6>,
 ]
 
 export function HeadingMenu({ editor }: { editor: Editor }) {
---
+++ src/constants.tsx
@@ -0,0 +1,13 @@
+export const EDITOR_SAMPLE = `
+    <h1>Typography should be easy</h1>
+    <p></p>
+    <p>So that's a header for you — with any luck if we've done our job correctly that will look pretty reasonable.</p>
+    <p></p>
+
+    <p><strong>Something a wise person once told me about typography is:</strong></p>
+    <p></p>
+
+    Typography is pretty important if you don't want your stuff to look like trash. Make it good then it won't be bad.
+
+    It's probably important that images look okay here by default as well:
+`
