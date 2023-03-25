ef1ea61a8b3650bd3b5d75571fcaa9507543fd4b
Mariam-maghraby <61349498+Mariam-maghraby@users.noreply.github.com>
2023-03-25 17:32:14 +0200

    feat: added basic sign up form with CSS

--- src/CreateStudent.tsx
+++ src/CreateStudent.tsx
@@ -1,3 +1,5 @@
+
+
 const CreateStudent = () => {
 
 
@@ -14,6 +16,16 @@ const CreateStudent = () => {
                 <label htmlFor="">Email:</label>
                 <input type="email"
                     required />
+                <label htmlFor="">Do you have any <span>disability</span></label>
+                <input type="radio" name="dis" id="" value="YES" />
+                <input type="radio" name="dis" id="" value="NO" />
+
+                <label htmlFor="">Disability</label>
+                <select name="" id="">
+                    <option value="visual">Visually Impaired</option>
+                    <option value="sighted">Partially Sighted</option>
+                    <option value="dyslexic">Dyslexia</option>
+                </select>
                 <button>Sign UP!</button>
             </form>
         </div>
