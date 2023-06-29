f27e1e6ba3b2a5ee041ba6aa9197181985a034eb
Mohamed-Ibrahim-01 <mohamed.ibrahim02023@gmail.com>
2023-06-29 11:53:03 +0300

    feat: styled login page

--- src/GlobalStyles.tsx
+++ src/GlobalStyles.tsx
@@ -1,19 +1,5 @@
 import { Global } from "@mantine/core"
-import nunito from "./assets/Nunito-VariableFont_wght.ttf"
 
 export function MyGlobalStyles() {
-    return (
-        <Global
-            styles={[
-                {
-                    "@font-face": {
-                        fontFamily: "nunito",
-                        src: `url('${nunito}') format("opentype")`,
-                        fontWeight: 500,
-                        fontStyle: "normal",
-                    },
-                },
-            ]}
-        />
-    )
+    return <Global styles={[]} />
 }
