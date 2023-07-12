49c648a75878a534154fba71ce94a12769b55259
Mohamed-Ibrahim-01 <mohamed.ibrahim02023@gmail.com>
2023-07-12 17:21:32 +0300

    fix: clean up

--- src/App.tsx
+++ src/App.tsx
@@ -52,7 +52,7 @@ const SharedBookView = (
     <CommonRouteMapper
         map={[
             { roles: ["AUTHOR", "ADMIN"], element: <BookEditor /> },
-            { roles: ["STUDENT"], element: <BookReader enableLineFocus /> },
+            { roles: ["STUDENT"], element: <BookReader /> },
         ]}
     />
 )
